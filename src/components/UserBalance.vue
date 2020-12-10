5d046b5042efa79b305433bf47e1ad2c08e040b6
Julie Padilla <julie.andrea.padilla@gmail.com>
2020-12-10 15:37:04 -0500

    corregida la ur

--- src/components/UserBalance.vue
+++ src/components/UserBalance.vue
@@ -18,7 +18,7 @@ export default {
     created: function(){
         this.username = this.$route.params.username
         let self = this
-    axios.get("http://127.0.0.1:8000/user/balance/" + this.username)
+    axios.get("https://cajero-app-julie.herokuapp.com/" + this.username)
         .then((result) => {
             self.balance = result.data.balance
         })
